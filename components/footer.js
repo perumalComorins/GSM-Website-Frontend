4c230a37efab6e8ae4e114b90881a5ac6d385bb0
dhayacomorins <89181141+dhayacomorins@users.noreply.github.com>
2022-04-12 13:36:41 +0530

    first commit

--- components/footer.js
+++ components/footer.js
@@ -1,55 +1,70 @@
-import { useContext, useEffect, useState } from 'react';
+import { userService } from '../services/user.service';
+import {  useEffect, useState } from 'react';
 import { Link } from "./Link";
 
 const Footer = (props) => {
+    const type='footer.php';
+    const [footerpanel, setFooterpanel] = useState([]);
+
+    useEffect( async() => {
+        
+        userService.getAllItems(type).then((res) => {    
+            setFooterpanel(res[0].json_data);
+        }) 
+         .catch((err) => console.error(err));         
+    }, []);
+
     return(
         <div className="container footercontainer container-70 reset-padding">
             <div className="footer-quotes d-none d-md-block">
-                <h2>Nos formations sont accessibles aux personnes en situation de handicap. 
-            Si vous avez des besoins spécifiques, n'hésitez pas à en informer notre référent handicap : Clément Berger de Nomazy sur l'adresse "relation-candidat@gsmmaster.fr"</h2>
+                <h2>{footerpanel.footer}</h2>
             </div>
             <div className="row reset-margin">
+            {footerpanel.footer_items  && footerpanel.footer_items.map((items, index) => 
                 <div className="col-sm-12 col-md-4 footer-widget reset-padding">
-                    <div className="row reset-margin">
-                        <div className="footerpanelSocialicons d-none d-md-block">
-                            <a href="#" className="footer-socialicon-link"><img src="/assets/images/linkedin-in-footer.png" className="icon" /></a>
-                            <a href="#" className="footer-socialicon-link"><img src="/assets/images/facebook-square-footer.png" className="icon" /></a>
+                    {items.desc &&
+                        <div className="row reset-margin">
+                            <div className="footerpanelSocialicons d-none d-md-block">
+                                <a href="#" className="footer-socialicon-link"><img src={items.footer_social_media && items.footer_social_media.linked_link} className="icon" /></a>
+                                <a href="#" className="footer-socialicon-link"><img src={items.footer_social_media && items.footer_social_media.facebook_link} className="icon" /></a>
+                            </div>
+                        
+                            <div className="footerpanelAboutContent">                         
+                                <h2 className="footer-widget-title">{items.footer_item_title}</h2>
+                                <p className="about-description">{items.desc}</p>
+                                <span className="footerpanelCopyright">{items.copyright}</span>
+                            </div>            
+                        </div> 
+                    } 
+                    {items.footer_sub_item &&
+                        <div>
+                            <h2 className="footer-widget-title">{items.footer_item_title}</h2>
+                            <ul className="footer-menu">
+                                {items.footer_sub_item.map((each, i) => 
+                                    <li key = {i} ><a href={each.link}>{each.title}</a></li>
+                                )}
+                            </ul>
                         </div>
-                        <div className="footerpanelAboutContent">
-                            <h2 className="footer-widget-title">GSM Master</h2>
-                            <p className="about-description">Present in the mobile telephony market since 1999, GSM Master founded its training center at the end of 2014
-                                with the aim of contributing to the standardization of the mobile product repair sector.</p>
-                            <span className="footerpanelCopyright">&copy; 2020 GSM Master. All rights reserved</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-12 col-md-4 footer-widget reset-padding">
-                    <h2 className="footer-widget-title">Further Information</h2>
-                    <ul className="footer-menu">
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Politique de confidentialité</a></li>
-                        <li><a href="#">Credits</a></li>
-                        <li><a href="#">CGU</a></li>
-                        <li><a href="#">Information légale</a></li>
-                        <li><a href="#">Plan du site</a></li>
-                    </ul>
-                </div>
-                <div className="col-sm-12 col-md-4 footer-widget reset-padding">
-                    <h2 className="footer-widget-title">Subscribe to Newsletter</h2>
-                    <form name="newsletter-form" className="newsletter">
-                        <div className="form-group">
-                            <input type="email" className="form-control border-0 newsletter-email" id="" aria-describedby="emailHelp" placeholder="Enter your email address here" />
-                        </div>
-                        <button type="button" className="btn gsm-bg-individual btn-gsm-sm">Subscribe</button>
-                        <span className="footerpanelSocialicons d-block d-md-none pull-right">
-                            <a href="#" className="footer-socialicon-link"><img src="/assets/images/awesome-facebook-square-mobile.png" className="icon" /></a>
-                            <a href="#" className="footer-socialicon-link"><img src="/assets/images/linkedin-in-footer-mobile.png" className="icon" /></a>
-                            
-                        </span>
-                    </form>
-                </div>
-            </div>
+                    } 
+                    {items.button_name &&
+                        <div>
+                            <h2 className="footer-widget-title">{items.footer_item_title}</h2>
+                            <form name="newsletter-form" className="newsletter">
+                                <div className="form-group">
+                                    <input type="email" className="form-control border-0 newsletter-email" id="" aria-describedby="emailHelp" placeholder={items.input_place_holder} />
+                                </div>
+                                <button type="button" className="btn gsm-bg-individual btn-gsm-sm">{items.button_name}</button>
+                                <span className="footerpanelSocialicons d-block d-md-none pull-right">
+                                    <a href="#" className="footer-socialicon-link"><img src="/assets/images/awesome-facebook-square-mobile.png" className="icon" /></a>
+                                    <a href="#" className="footer-socialicon-link"><img src="/assets/images/linkedin-in-footer-mobile.png" className="icon" /></a>                            
+                                </span>
+                            </form>
+                        </div> 
+                    } 
+                </div> 
+            )}
+                                                                                         
         </div>
-    )
+    </div>)
 }
 export default Footer;
