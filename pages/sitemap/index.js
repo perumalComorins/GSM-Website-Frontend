a422cf12d799b0b86d83aab1877fee468c138567
perumalComorins <86822193+perumalComorins@users.noreply.github.com>
2022-04-21 17:25:17 +0530

    Site Map 21-02-22
    Site Map 21-02-22

--- pages/sitemap/index.js
+++ pages/sitemap/index.js
@@ -7,9 +7,10 @@ import Footer from "../../components/footer";
 
 export default function Sitemap(){
 
-    const type='blog.php'
-    // const [sitemap, setSiteMap]=useState([]);
-    // const [loading,setLoading] = useState(true);
+    const type='sitemap.php'
+    const [sitemap, setSiteMap]=useState([]);
+    const [sitemapBanner, setSiteMapBanner] = useState(null);
+    const [loading,setLoading] = useState(true);
 
 
     useEffect( async() => {
@@ -18,11 +19,13 @@ export default function Sitemap(){
         body_ele.className='';
         body_ele.classList.add("inner-page");
         userService.getAllItems(type).then((res) => {
-            setBlog(res.json_data);
+            setSiteMap(res.json_data.sitemap);
+            setSiteMapBanner(res.json_data.sitemapimage);
             setLoading(false);
         }) 
+         
     }, []);
-
+    console.log(`SiteMap`,sitemap);
     return(
         <div id="wrapper">
             <div className="overlay"></div>
@@ -44,59 +47,28 @@ export default function Sitemap(){
                             <div className="col-md-8 reset-padding align-self-center">
                                 <div class="sitebar-row">
                                     <ul class="parent-list reset-padding">
+                                        {sitemap && sitemap.map((items, index) => 
                                         <li class="sitebar-box">
-                                            <h2>Main Content</h2>
+                                            <h2>{items.title}</h2>
                                             <ul>
-                                            <li><a href="index.php">Home</a></li>
-                                            <li>The Body</li>
-                                            <li><a href="contact-faq.php">Contact &amp; FAQ</a></li>
-                                            <li><a href="certificate.php">Certification</a></li>
-                                            <li><a href="recritment.php">Career</a></li>
-                                            <li><a href="blog.php">Blog</a></li>
+                                                {items.menu && items.menu.map((each_item, i) => 
+                                                <li><a href={each_item.link}>{each_item.list}</a></li>)}
                                             </ul>
-                                        </li>
-                                        <li class="sitebar-box">
-                                            <h2>Individual</h2>
-                                            <ul>
-                                            <li><a href="individual.php">Formations</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="sitebar-box">
-                                            <h2>Company</h2>
-                                            <ul>
-                                            <li><a href="qualified_staff.php">Team recruitment</a></li>
-                                            <li><a href="qualified_staff.php">Upgrade Team</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="sitebar-box">
-                                            <h2>Organization</h2>
-                                            <ul>
-                                            <li><a href="organisation.php">Registration - Organization</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="sitebar-box">
-                                            <h2>Connections</h2>
-                                            <ul>
-                                            <li><a href="terms-privacy.php">CGUG</a></li>
-                                            <li><a href="legal-notice.php">Credits</a></li>
-                                            <li><a href="terms-privacy.php">Legal Notice</a></li>
-                                            <li><a href="terms-privacy.php">DPR</a></li>
-                                            <li><a href="sitemap.php">Sitemap</a></li>
-                                            </ul>
-                                        </li>
+                                        </li>)}
                                     </ul>
                                 </div>
                             </div>
                             <div className="col-md-4 reset-padding">
-                                <img src="images/information-architecture.png" className="img-fluid"/>
+                                <img src={sitemapBanner} className="img-fluid"/>
                             </div>
                         </div>
                     </div>
                 </section>
+                <footer className="site-footer">
+                    <Footer />
+                </footer>  
             </div>
-            <footer className="site-footer">
-                <Footer />
-            </footer>  
+            
         </div>
         
     )
